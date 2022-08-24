69129e7d5a480c30bf6d5aeef618bf05c4f0af39
Dnyaneshwari Shinde <dnyaneshwari2400@gmail.com>
2022-08-24 16:45:51 +0530

    first commit

---
+++ src/main/java/com/cognizant/exception/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package com.cognizant.exception;
+
+public class InvalidTokenException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTokenException(String message) {
+		super(message);
+	}
+}
